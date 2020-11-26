f538dd618663cdec661276345d96c0bcb725d09d
ah-james <11ahj11@gmail.com>
2020-11-26 15:56:20 -0500

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,10 +1,9 @@
 class Application
     def call(env)
         resp = Rack::Response.new
-        current_time = Time.now
-        time_in_hours = current_time.strftime("%k%M")
-        resp.write "It is currently #{time_in_hours}"
-        if time_in_hours < "1200"
+        current_time = Time.now.strftime("%k:%M")
+        resp.write "It is currently #{current_time}"
+        if current_time < "12:00"
             resp.write "\nGood Morning!"
         else
             resp.write "\nGood Afternoon!"
